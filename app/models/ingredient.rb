f7dfdc005e91deac2f76e5c3c5fa76d0a5ecac66
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-08 22:46:55 +0000

    Automatically backed up by Learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,3 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
-  
-  def ingredient_attributes=(ingredient)
-    self.ingredient = Ingredient.find_or_create_by(name: ingredient[:name], quantity: ingredient[:quantity])
-    self.ingredient.update(ingredient)
-  end
-  
 end
