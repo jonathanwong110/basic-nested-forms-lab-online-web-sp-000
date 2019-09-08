7c6abab6ae52884eb255e79f1eb0b3dfcae4b5cc
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-08 21:48:40 +0000

    Automatically backed up by Learn

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,24 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path
+  end
+  
+  private
+  
+  def recipe_params
+  end
+  
+end
