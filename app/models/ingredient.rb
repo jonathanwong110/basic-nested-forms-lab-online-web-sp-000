7d7616fe32a4e05c6bc9e666966773d16ad3f227
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-08 22:33:51 +0000

    Automatically backed up by Learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,9 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  
+  def ingredient_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist[:name])
+    self.artist.update(artist)
+  end
+  
 end
