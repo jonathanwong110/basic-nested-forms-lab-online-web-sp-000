2f233e33333ffc1490eecbc94cabdeeaf2eea89a
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-08 21:49:55 +0000

    Automatically backed up by Learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,24 +1,2 @@
 class IngredientsController < ApplicationController
-  def show
-    @ingredient = Ingredient.find(params[:id])
-  end
-
-  def index
-    @ingredients = Ingredient.all
-  end
-
-  def new
-    @ingredient = Ingredient.new
-  end
-
-  def create
-    ingredient = Ingredient.create(ingredient_params)
-    redirect_to ingredient_path
-  end
-  
-  private
-  
-  def ingredient_params
-  end
-  
 end
